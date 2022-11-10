9d0b375fa3d499f1c3bcb3e7dbcaed2ac3f14af7
Raheem Kawojue <kawojue08@gmail.com>
2022-11-10 13:27:25 +0100

    font & colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./index.html', './src/**/*.jsx'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ["Poppins", 'sans-serif']
+      },
+      colors: {
+        'pry-clr-0': 'rgb(241, 245, 248)',
+        'pry-clr-1': 'rgb(6, 50, 81)',
+        'pry-clr-2': 'rgb(97, 125, 152)',
+        'pry-clr-3': 'rgb(73, 166, 233)',
+        'pry-clr-4': 'rgb(73, 166, 233)',
+        'pry-clr-4': 'rgb(165, 213, 248)',
+      }
+    },
   },
   plugins: [],
 }
