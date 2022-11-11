82543d1e515cc9b9e9ac4fc70cce5ac333187f6a
Raheem Kawojue <kawojue08@gmail.com>
2022-11-11 17:10:48 +0100

    added context

---
+++ src/components/Context.jsx
@@ -0,0 +1,29 @@
+import data from '../data'
+import { useRef, useState, useEffect, createContext } from 'react'
+
+const Context = createContext({})
+
+export const DataProvider = ({ children }) => {
+
+    const input = useRef(0)
+    const [texts, setTexts] = useState([])
+
+    useEffect(() => {
+        input.current.value = 0
+    }, []) // becomes zero once the page reloads.
+
+    const submit = (e) => {
+        e.preventDefault()
+        setTexts(data.slice(0, parseInt(input.current.value)))
+    }
+
+    return (
+        <Context.Provider value={{
+            submit, input, texts
+        }}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export default Context
