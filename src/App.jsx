87a3d26aed51afb19c4c2795405ae944abc40280
Raheem Kawojue <kawojue08@gmail.com>
2022-11-11 16:07:06 +0100

    initialize input with useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import texts from './data'
+import { useRef, useEffect } from 'react'
+import Input from './components/Input'
+
 const App = () => {
+  const input = useRef(0)
+
+  useEffect(() => {
+    input.current.value = 0;
+  }, []) // zero once the page reloads.
+
   return (
     <main>
-      <p>Greetings - Hello, World</p>
+      <header>
+        <h1>generate dummy texts</h1>
+      </header>
+      <Input texts={texts} input={input} />
     </main>
   )
 }
