39a2faaa0f9d942f66e955883f763552337368b2
Raheem Kawojue <kawojue08@gmail.com>
2022-11-11 18:23:21 +0100

    copy btn

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ import Context from './Context'
 import { useContext } from 'react'
 
 const Input = () => {
-    const { input, submit } = useContext(Context)
+    const { texts, input, submit, onCopy, copy } = useContext(Context)
 
     return (
         <form onSubmit={(e) => submit(e)}>
@@ -11,6 +11,9 @@ const Input = () => {
                 <input type="number" ref={input} min={0} max={9} autoFocus id="size" />
             </div>
             <button className="btn">generate</button>
+            <button className="btn" onClick={() => onCopy(texts.join(''))}>
+                {copy}
+            </button>
         </form>
     )
 }
