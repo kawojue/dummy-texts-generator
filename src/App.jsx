e57710983a14e86a8d6e3c8b503ebb4284b495df
Raheem Kawojue <kawojue08@gmail.com>
2022-11-11 22:40:04 +0100

    refractor

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useContext } from 'react'
 import Input from './components/Input'
-import Paragraphs from './components/Paragraphs'
+import Context from './components/Context'
+import Paragraph from './components/Paragraph'
 
 const App = () => {
+  const { texts } = useContext(Context)
+
   return (
     <main>
       <header>
-        <h1 className="md:text-4xl">generate dummy texts</h1>
+        <h1 className="md:text-4xl">
+          generate dummy texts
+        </h1>
       </header>
       <Input />
-      <Paragraphs />
+      <section>
+        {texts.length !== 0 ?
+          <article className="paragraph">
+            {texts.map((text, index) => (
+              <Paragraph text={text} key={index} />
+            ))}
+          </article> :
+          <article className="paragraph">
+            <p className="p-tag md:text-[0.9rem]">
+              No text.
+            </p>
+          </article>}
+      </section>
     </main>
   )
 }
